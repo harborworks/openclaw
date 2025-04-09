6cb3658ab51e4fdd43d499e0952c2dfe472a8ee0
sparrowml <ben@sparrow.dev>
2025-04-09 10:09:09 -0500

    fix bad migration

--- drizzle/0005_windy_the_hood.sql
+++ drizzle/0005_windy_the_hood.sql
@@ -1,8 +1,11 @@
-ALTER TABLE "tags" DROP CONSTRAINT "tags_deleted_by_id_users_id_fk";
+ALTER TABLE "tags" DROP CONSTRAINT IF EXISTS "tags_deleted_by_id_users_id_fk";
 --> statement-breakpoint
-ALTER TABLE "tasks" DROP CONSTRAINT "tasks_deleted_by_id_users_id_fk";
+ALTER TABLE "tasks" DROP CONSTRAINT IF EXISTS "tasks_deleted_by_id_users_id_fk";
 --> statement-breakpoint
-ALTER TABLE "tags" DROP COLUMN "deleted_by_id";--> statement-breakpoint
-ALTER TABLE "tags" DROP COLUMN "deleted_at";--> statement-breakpoint
-ALTER TABLE "tasks" DROP COLUMN "deleted_by_id";--> statement-breakpoint
-ALTER TABLE "tasks" DROP COLUMN "deleted_at";
+ALTER TABLE "tags" DROP COLUMN IF EXISTS "deleted_by_id";
+--> statement-breakpoint
+ALTER TABLE "tags" DROP COLUMN IF EXISTS "deleted_at";
+--> statement-breakpoint
+ALTER TABLE "tasks" DROP COLUMN IF EXISTS "deleted_by_id";
+--> statement-breakpoint
+ALTER TABLE "tasks" DROP COLUMN IF EXISTS "deleted_at";
