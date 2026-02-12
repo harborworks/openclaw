4d62a53cbecaf36a685c8f858f30b09224422d3f
Frodo <frodo@sparrow.dev>
2026-02-12 15:38:14 -0600

    Replace dice emoji with refresh arrows icon for random name button

--- frontend/src/pages/AgentsPage.tsx
+++ frontend/src/pages/AgentsPage.tsx
@@ -96,7 +96,10 @@ function AgentForm({
               title="Random name"
               onClick={() => setName(randomAgentName(existingNames))}
             >
-              🎲
+              <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M21.5 2v6h-6" /><path d="M2.5 22v-6h6" />
+                <path d="M2.5 11.5a10 10 0 0 1 18.4-4.5" /><path d="M21.5 12.5a10 10 0 0 1-18.4 4.5" />
+              </svg>
             </button>
           </div>
         </label>
